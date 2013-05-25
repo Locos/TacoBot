367e4d0e0cdba879b22fb576a64a611a19a4d55c
vemacs <gentoo@outlook.com>
2013-05-25 16:29:35 -0600

    Added SRV record support

--- src/net/esper/tacos/ircbot/commands/mcping.java
+++ src/net/esper/tacos/ircbot/commands/mcping.java
@@ -1,5 +1,10 @@
 package net.esper.tacos.ircbot.commands;
 
+import java.util.Hashtable;
+import java.util.jar.Attributes;
+
+import javax.naming.directory.InitialDirContext;
+
 import net.esper.tacos.ircbot.ICommand;
 import net.esper.tacos.ircbot.Rank;
 import net.esper.tacos.ircbot.TacoBot;
@@ -12,12 +17,15 @@ public class mcping implements ICommand {
 	@Override
 	public void exec(String message, String[] args, User user) {
 		if (args.length != 2) {
-			TacoBot.sendMessage(user, "Usage: " + TacoBot.PREFIX + "mcping [ip](:port)");
+			TacoBot.sendMessage(user, "Usage: " + TacoBot.PREFIX
+					+ "mcping [ip](:port)");
 			return;
 		}
 		String arg1 = args[1];
-		int arg2 = (arg1.split(":").length > 1 ? Integer.parseInt(arg1.split(":")[1]) : 25565);
-		String res = Utils.getFormattedPing((arg1.split(":").length > 1 ? arg1.split(":")[0] : arg1), arg2);
+		int arg2 = (arg1.split(":").length > 1 ? Integer.parseInt(arg1
+				.split(":")[1]) : Integer.parseInt(getServerPort(arg1)));
+		String res = Utils.getFormattedPing(
+				(arg1.split(":").length > 1 ? arg1.split(":")[0] : arg1), arg2);
 		if (res == null) {
 			TacoBot.sendMessage(user, "Sorry, I could not ping " + arg1 + ".");
 			return;
@@ -33,5 +41,22 @@ public class mcping implements ICommand {
 	@Override
 	public String getNoAccessMessage() {
 		return null;
+	}
+
+	private static String getServerPort(String par0Str) {
+		try {
+			Class.forName("com.sun.jndi.dns.DnsContextFactory");
+			Hashtable var2 = new Hashtable();
+			var2.put("java.naming.factory.initial",
+					"com.sun.jndi.dns.DnsContextFactory");
+			var2.put("java.naming.provider.url", "dns:");
+			InitialDirContext var3 = new InitialDirContext(var2);
+			javax.naming.directory.Attributes var4 = var3.getAttributes(
+					"_minecraft._tcp." + par0Str, new String[] { "SRV" });
+			String[] var5 = var4.get("srv").get().toString().split(" ", 4);
+			return var5[2];
+		} catch (Throwable var6) {
+			return Integer.toString(25565);
+		}
 	}
 }
--- src/net/esper/tacos/ircbot/utils/mcping/MinecraftPing.java
+++ src/net/esper/tacos/ircbot/utils/mcping/MinecraftPing.java
@@ -8,28 +8,42 @@ import java.net.Socket;
 
 // Thanks jamietech!
 public class MinecraftPing {
-	public static MinecraftPingReply getOldPing(final String response, final String hostname, final int port) throws IOException {
+	public static MinecraftPingReply getOldPing(final String response,
+			final String hostname, final int port) throws IOException {
 		final String[] bits = response.split("\u00a7");
-		if (bits.length != 3) { throw new IOException("Bad message - The OLD ping was not specified properly."); }
-		return new MinecraftPingReply(hostname, port, bits[0], Integer.valueOf(bits[2]), Integer.valueOf(bits[1]));
+		if (bits.length != 3) {
+			throw new IOException(
+					"Bad message - The OLD ping was not specified properly.");
+		}
+		return new MinecraftPingReply(hostname, port, bits[0],
+				Integer.valueOf(bits[2]), Integer.valueOf(bits[1]));
 	}
-	
-	public static MinecraftPingReply getPing(final String hostname, final int port) throws IOException {
+
+	public static MinecraftPingReply getPing(final String hostname,
+			final int port) throws IOException {
 		Socket socket = new Socket();
 		socket.connect(new InetSocketAddress(hostname, port), 3000);
 		DataInputStream in = new DataInputStream(socket.getInputStream());
 		DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 		out.write(254);
 		out.writeByte(1);
-		if (in.read() != 255) { throw new IOException("Bad message - An incorrect packet was received."); }
+		if (in.read() != 255) {
+			throw new IOException(
+					"Bad message - An incorrect packet was received.");
+		}
 		final short bit = in.readShort();
 		final StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < bit; ++i) {
 			sb.append(in.readChar());
 		}
 		out.close();
+		socket.close();
+
 		final String[] bits = sb.toString().split("\0");
-		if (bits.length != 6) { return getOldPing(sb.toString(), hostname, port); }
-		return new MinecraftPingReply(hostname, port, bits[3], bits[1], bits[2], Integer.valueOf(bits[5]), Integer.valueOf(bits[4]));
+		if (bits.length != 6) {
+			return getOldPing(sb.toString(), hostname, port);
+		}
+		return new MinecraftPingReply(hostname, port, bits[3], bits[1],
+				bits[2], Integer.valueOf(bits[5]), Integer.valueOf(bits[4]));
 	}
 }
