a870d0dd3e5bed6848487c28d70d2e9c0325112e
vemacs <gentoo@outlook.com>
2013-05-25 14:19:33 -0600

    SnakeYAML

--- src/net/esper/tacos/ircbot/TacoBot.java
+++ src/net/esper/tacos/ircbot/TacoBot.java
@@ -1,26 +1,32 @@
 package net.esper.tacos.ircbot;
 
-import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.io.InputStream;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
-import org.pircbotx.*;
+
+import org.pircbotx.Channel;
+import org.pircbotx.PircBotX;
+import org.pircbotx.User;
+import org.pircbotx.UtilSSLSocketFactory;
 import org.pircbotx.hooks.Listener;
 import org.pircbotx.hooks.ListenerAdapter;
 import org.pircbotx.hooks.events.MessageEvent;
 import org.pircbotx.hooks.events.PrivateMessageEvent;
+import org.yaml.snakeyaml.Yaml;
 
 @SuppressWarnings("rawtypes")
 public class TacoBot extends ListenerAdapter implements Listener {
-	
+
 	public static String CHAN = "#tacos";
-	public static String NICK = "Projectile";
+	public static String NICK = "TacoBot";
 	public static Channel CHAN_OBJ;
 	public static final String PREFIX = ".";
 	public static final PircBotX bot = new PircBotX();
-	
+
 	public static void main(String[] args) throws Exception {
 		// Setup
 		CommandProcessor.init();
@@ -28,65 +34,63 @@ public class TacoBot extends ListenerAdapter implements Listener {
 		// bot.setVerbose(true);
 		bot.setVersion("TacoBot v1.2 - For #tacos only!");
 		bot.getListenerManager().addListener(new TacoBot());
-		
+
 		// Now connect the magic
 		try {
-			BufferedReader br = new BufferedReader(new FileReader("pass.wd"));
-			String server = br.readLine();
-			int port = Integer.parseInt(br.readLine());
-			String line, password = null, channel = null;
+			InputStream input = new FileInputStream(new File("config.yml"));
+		    Yaml yaml = new Yaml();
+		    Map config = (Map) yaml.load(input);
 			boolean ssl = false;
-			while ((line = br.readLine()) != null) {
-				if (line.startsWith("#") && channel == null) {
-					channel = line;
-				} else if (password == null) {
-					password = line;
-				} else if (line.equalsIgnoreCase("ssl")) {
-					ssl = true;
-				}
-			}
+
+			String server = (String) config.get("server");
+			int port = Integer.parseInt((String) config.get("port"));
+			String password = (String) config.get("password");
+			String channel = (String) config.get("channel");
+			ssl = (Boolean) config.get("ssl");
+
 			if (ssl) {
 				// TODO: Figure out Java Keystore to only trust bouncer
 				// certificate.
-				bot.connect(server, port, password, new UtilSSLSocketFactory().trustAllCertificates());
+				bot.connect(server, port, password,
+						new UtilSSLSocketFactory().trustAllCertificates());
 			} else {
 				bot.connect(server, port, password);
 			}
 			CHAN = channel;
 			CHAN_OBJ = bot.getChannel(channel);
-			br.close();
+			input.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			System.out.println("Connected to Spigot Tacos!");
 		}
 	}
-	
+
 	private static List<String> msgs = new CopyOnWriteArrayList<String>();
-	
+
 	public static void addMsg(String msg) {
 		msgs.add(msg);
 		if (msgs.size() == 6) {
 			msgs.remove(0);
 		}
 	}
-	
+
 	public static String[] getMsgs() {
 		return msgs.toArray(new String[0]);
 	}
-	
+
 	public static void sendMessage(User user, String message) {
 		sendMessage(user.getNick(), message);
 	}
-	
+
 	public static void sendMessage(String user, String message) {
 		sendMessage("(" + user + ") " + message);
 	}
-	
+
 	public static void sendMessage(String message) {
 		bot.sendMessage(CHAN, message);
 	}
-	
+
 	@Override
 	public void onMessage(MessageEvent event) throws Exception {
 		if (!event.getChannel().getName().equals(CHAN)) {
@@ -95,14 +99,14 @@ public class TacoBot extends ListenerAdapter implements Listener {
 		addMsg("<" + event.getUser().getNick() + "> " + event.getMessage());
 		CommandProcessor.process(event);
 	}
-	
+
 	@Override
 	public void onPrivateMessage(PrivateMessageEvent event) {
 		if (!event.getUser().getChannelsOpIn().contains(CHAN_OBJ)) {
 			event.respond("Sorry, but this bot does not support private messaging capabilities.");
 			return;
 		}
-		
+
 		CommandProcessor.process(event);
 	}
 }
